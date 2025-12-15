6eedc71f427e46961506b4a6a3ec35d06d9f1d64
nycolaide <laurent@nycolaide.dev>
2025-12-15 16:03:40 +0100

    initialize new lili-core

--- packages/lapikit/src/lib/entry-bundler.ts
+++ packages/lapikit/src/lib/entry-bundler.ts
@@ -1,5 +1,14 @@
 function lili() {
 	console.log('Lapikit preprocess running...');
+
+	return {
+		markup({ content }: { content: string; filename?: string }) {
+			const processedContent = content;
+			return {
+				code: processedContent
+			};
+		}
+	};
 }
 
 export { lili };
